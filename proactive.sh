ecf18eff29c84de9c6a007563d37d12da4e467c5
username <jimmy.woodard@comcast.net>
2018-08-02 02:39:53 -0600

    added SQL/gzip/xz routines for /var/www/ /home/

--- proactive.sh
+++ proactive.sh
@@ -12,14 +12,27 @@ postfixmailqueue="$(mailq | tail -n1 | awk '{print $5}')"
 #Uptime
 echo "Uptime:"
 echo "$uptime"
+echo
 
 #Free Memory
 echo "Free Memory:"
 echo "$freemem"
+echo
 
 #Disk Space
 echo "Disk Space:"
 echo "$freedisk"
+echo
+
+#backup files within docroots
+echo "Gzip, xz and SQL files within docroots:"
+find /var/www/*/htdocs/ -name "*.gz" -size +1M -o -name "*.xz" -size +1M -o -name "*.sql" -size +1M | grep -v 'backup_migrate'
+echo
+
+#Uncompressed sql files within /home/bmesh/ and /home/bmesh_admin
+echo "Uncompressed sql files:"
+find /home/bmesh_admin /home/bmesh/ -name "*.sql" -size +1M
+echo
 
 #Mail Queue
 if pgrep -x "master" > /dev/null;
@@ -31,20 +44,18 @@ then
 else
     echo "Unknown Mail System"
 fi
+echo
 
-#CMS Updates listing Routine
-
+#CMS Updates Listing Routine
 mkdir -p /opt/scripts/
-rm -rf /opt/scripts/updates.txt
-
-echo "Docroots found in /etc/httpd/conf.d/*.conf:" >> /opt/scripts/updates.txt
-echo "$docroots" >> /opt/scripts/updates.txt
-
-for docroot in $docroots; do echo ; cd $docroot ; echo $(pwd) ; wp core version  --allow-root 2>/dev/null ; wp plugin list --allow-root 2>/dev/null | grep -i 'available' ; wp theme list --allow-root 2>/dev/null | grep -i 'available' ; drush status 2>/dev/null | grep -i 'Drupal version'; drush up --security-only -n 2>/dev/null | grep -i 'SECURITY UPDATE available' ; done >> /opt/scripts/updates.txt
+rm -rf /opt/scripts/cms_updates.txt
+echo "Docroots found in /etc/httpd/conf.d/*.conf:" >> /opt/scripts/cms_updates.txt
+echo "$docroots" >> /opt/scripts/cms_updates.txt
+for docroot in $docroots; do echo ; cd $docroot ; echo $(pwd) ; wp core version  --allow-root 2>/dev/null ; wp plugin list --allow-root 2>/dev/null | grep -i 'available' ; wp theme list --allow-root 2>/dev/null | grep -i 'available' ; drush status 2>/dev/null | grep -i 'Drupal version'; drush up --security-only -n 2>/dev/null | grep -i 'SECURITY UPDATE available' ; done >> /opt/scripts/cms_updates.txt
 
 finish_time="$(date +%s)"
 
-#Send Results Via Mail
+#Send Results Via Mail - commented out for testing
 #mail -s 'CMS updates for $hostname' jwoodard@contegix.com < /opt/scripts/updates.txt
 
 echo "Time duration: $((finish_time - start_time)) secs."
