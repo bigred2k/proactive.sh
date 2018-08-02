6bc952b5c7fb7be28f125d5ae294a5cde97801a6
username <jimmy.woodard@comcast.net>
2018-08-02 03:14:51 -0600

    random script cleanup

--- proactive.sh
+++ proactive.sh
@@ -2,12 +2,16 @@
 
 start_time="$(date +%s)"
 uptime="$(uptime)"
-freemem="$(free -m)"
+freemem="$(free -g)"
 freedisk="$(df -h | grep '/'|sort -nr -k5)"
 docroots="$(cat /etc/httpd/conf.d/*.conf |grep DocumentRoot | grep -v '#'|awk '{print $2}'|sort |uniq)"
 hostname="$(hostname)"
 sendmailqueue="$(mailq | tail -n1 | awk '{print $3}')"
 postfixmailqueue="$(mailq | tail -n1 | awk '{print $5}')"
+
+#Hostname
+echo "Hostname: $hostname"
+echo
 
 #Uptime
 echo "Uptime:"
@@ -15,7 +19,7 @@ echo "$uptime"
 echo
 
 #Free Memory
-echo "Free Memory:"
+echo "Memory Usage (Gigabytes):"
 echo "$freemem"
 echo
 
@@ -51,7 +55,10 @@ mkdir -p /opt/scripts/
 rm -rf /opt/scripts/cms_updates.txt
 echo "Docroots found in /etc/httpd/conf.d/*.conf:" >> /opt/scripts/cms_updates.txt
 echo "$docroots" >> /opt/scripts/cms_updates.txt
-for docroot in $docroots; do echo ; cd $docroot ; echo $(pwd) ; wp core version  --allow-root 2>/dev/null ; wp plugin list --allow-root 2>/dev/null | grep -i 'available' ; wp theme list --allow-root 2>/dev/null | grep -i 'available' ; drush status 2>/dev/null | grep -i 'Drupal version'; drush up --security-only -n 2>/dev/null | grep -i 'SECURITY UPDATE available' ; done >> /opt/scripts/cms_updates.txt
+echo "Checking for Drupal/Wordpress updates. This could take awhile. Please be patient."
+for docroot in $docroots; do echo ; cd "$docroot" || exit ; pwd ; wp core version  --allow-root 2>/dev/null ; wp plugin list --allow-root 2>/dev/null | grep -i 'available' ; wp theme list --allow-root 2>/dev/null | grep -i 'available' ; drush status 2>/dev/null | grep -i 'Drupal version'; drush up --security-only -n 2>/dev/null | grep -i 'SECURITY UPDATE available' ; done >> /opt/scripts/cms_updates.txt
+
+echo "CMS updates scanning complete. Results will be in /opt/scripts/cms_updates.txt"
 
 finish_time="$(date +%s)"
 
